5eab2ccc8bb533f11ccd95bfcaa8e6eaa0f62934
yesmider <ftchinet>
2017-06-27 18:39:31 +0800

    dev 1.0.8 improve stone compare accuracy

--- UIsync.py
+++ UIsync.py
@@ -308,18 +308,24 @@ class Stone_UI:
                 for m, n in matches:
                     if m.distance < 0.4 * n.distance:
                         good.append(m)
+                homography = []
                 if len(good) >=5:
+                    src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
+                    dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
+                    M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+                    maskmatches = mask.ravel().tolist()
+                    for index,pts in enumerate(good):
+                        if maskmatches[index] == 1:
+                            homography.append(pts)
+                if len(homography) >=5:
                     good_index.append(self.x_y_to_pixel(x,y))
-                    # src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
-                    # dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
-                    # M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
-                    # print(len(good),M,len(mask))
+
                     # draw_params = dict(matchColor=(0, 255, 0),  # draw matches in green color
                     #                    singlePointColor=None,
                     #                    matchesMask=None,  # draw only inliers
                     #                    flags=2)
                     #
-                    # img3 = cv2.drawMatches(stone, kp1, pic, kp2, good, None, **draw_params)
+                    # img3 = cv2.drawMatches(stone, kp1, pic, kp2, homography, None, **draw_params)
                     # cv2.imshow('image', img3)
                     # cv2.waitKey(0)
                     # cv2.destroyAllWindows()
@@ -524,10 +530,6 @@ class UI_controll(Stone_UI):
         while 1:
             if self.check_game_active() is True:
                 try:
-                    if self.buster == 0:
-                        ran = random.randint(2, 30)
-                    else:
-                        ran = 2
                     self.img_refresh()
                     if self.check_mining_or_mob() == 'mining':
                         if self.check_rain() is True:
@@ -538,6 +540,10 @@ class UI_controll(Stone_UI):
                                 fast_mining_time = time.time()
                         else:
                             self.buster = 0
+                        if self.buster == 0:
+                            ran = random.randint(2, 15)
+                        else:
+                            ran = 2
                         self.close_pop_box()
                         self.Turn_on_auto_attack()
                         # self.Clan_exp_up()
@@ -547,6 +553,7 @@ class UI_controll(Stone_UI):
                         self.stone_combine()
                         time.sleep(ran)
                     else:
+                        ran = random.randint(2, 15)
                         self.buster = 0
                         self.close_pop_box()
                         self.Turn_on_auto_attack()
